b3410c1e42d5c360c321f904f114b1cf3f27b364
12220143@utehy.edu.vn <12220143@utehy.edu.vn>
2023-12-15 00:41:12 +0700

    a

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
         <h1>Sang đẹp trai</h1>
         <a href="https://www.facebook.com/profile.php?id=100020967847527"></a>
         <p>
-          Ngày sinh 21
+          Ngày sinh 21-05-2002
 
         </p>
     </div>
