aece0c2471e6f094beeb76f18156767f040c484a
12220143@utehy.edu.vn <12220143@utehy.edu.vn>
2023-12-25 23:51:56 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import React from "react";
+
+const postItem = React.createElement("div",
+  {
+    className: "post-item",
+    color: "#fff",
+  },
+  React.createElement("h2",{
+    title:"Hoc React tai F8"
+
+  },"Hoc ReactJS"),
+  React.createElement("p",null,"ReactJS từ cơ bản đến nâng cao")
+
+)
+
+console.log(postItem)
 function App() {
   return (
     <div className="App">
@@ -5,7 +21,13 @@ function App() {
         <a href="https://www.facebook.com/profile.php?id=100020967847527"></a>
         <p>
           Ngày sinh 21-05-2002 
-          Sở thihcs 123
+         
+        </p>
+        <p>
+            Sở thích: code
+        </p>
+        <p>
+          Người yêu: Không có
         </p>
     </div>
   );
